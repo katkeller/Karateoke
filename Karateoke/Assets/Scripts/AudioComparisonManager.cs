b0972b20978d4b12976f2ceb8bfe6a317af90bf8
Kat Keller <kat.kek.keller@gmail.com>
2019-11-02 14:00:12 -0500

    Added player 2 fuctionality and reorganized code in AudioComparisonManager

--- Karateoke/Assets/Scripts/AudioComparisonManager.cs
+++ Karateoke/Assets/Scripts/AudioComparisonManager.cs
@@ -6,61 +6,124 @@ using TMPro;
 
 public class AudioComparisonManager : MonoBehaviour
 {
-    [SerializeField]
-    private GameObject player1AudioSampleObject, player2AudioSampleObject, sourceAudioSampleObject;
+    //[SerializeField]
+    //private GameObject player1AudioSampleObject, player2AudioSampleObject, sourceAudioSampleObject;
 
     [SerializeField]
-    private TextMeshProUGUI player1ScoreText, player1DifferenceText;
+    private GameObject[] audioSampleObject = new GameObject[3];
 
-    private AudioSampleCollector player1AudioSampleCollector, sourceAudioSampleCollector;
+    [SerializeField]
+    private TextMeshProUGUI player1ScoreText, player2ScoreText, player1DifferenceText, player2DifferenceText;
+
+    [SerializeField]
+    private float[] phraseEndTimes = new float[4];
+
+    //private AudioSampleCollector player1AudioSampleCollector, player2AudioSampleCollector, sourceAudioSampleCollector;
+    //private float differenceBetweenPlayer1AndSource = 0.0f, differenceBetweenPLayer2AndSource = 0.0f;
+    //private int player1Index, player2Index, sourceIndex;
+    //private float player1HighestValue, player2HighestValue, sourceHighestValue;
+
+    private AudioSampleCollector[] audioSampleCollector = new AudioSampleCollector[3];
+    private int[] index = new int[3];
+    private float[] highestValue = new float[3];
+
+    private float[] differenceBetweenPlayerAndSource = new float[2];
+    private float[] playerScore = new float[2];
     private bool atEndOfPhrase;
-    private float differenceBetweenPlayer1AndSource = 0.0f;
-    private int player1Index, sourceIndex;
-    private float player1HighestValue, sourceHighestValue;
-
-    private float player1Score = 0.0f, player2Score = 0.0f;
+    private bool recordingValues;
 
     private void Awake()
     {
-        player1AudioSampleCollector = player1AudioSampleObject.GetComponent<AudioSampleCollector>();
+
+        //player1AudioSampleCollector = player1AudioSampleObject.GetComponent<AudioSampleCollector>();
         //player2AudioSampleCollector = player2AudioSampleObject.GetComponent<AudioSampleCollector>();
-        sourceAudioSampleCollector = sourceAudioSampleObject.GetComponent<AudioSampleCollector>();
+        //sourceAudioSampleCollector = sourceAudioSampleObject.GetComponent<AudioSampleCollector>();
+
+        for (int i = 0; i < 3; i++)
+        {
+            audioSampleCollector[i] = audioSampleObject[i].GetComponent<AudioSampleCollector>();
+        }
     }
 
     void Start()
     {
-        
+        highestValue[0] = audioSampleCollector[0].highestValue;
+        recordingValues = true;
     }
 
     void Update()
     {
-        if (!atEndOfPhrase)
+        //if (sourceHighestValue < 0.1f)
+        //    recordingValues = false;
+        //else
+        //    recordingValues = true;
+
+        //if (!atEndOfPhrase && recordingValues)
+        //{
+        //    RecordSampleValues();
+        //    UpdateText();
+        //}
+
+        //if (highestValue[0] < 0.1f)
+        //    recordingValues = false;
+        //else
+        //    recordingValues = true;
+
+        if (!atEndOfPhrase && recordingValues)
         {
-            RecordSampleValues();
-            UpdateText();
+            for (int e = 0; e < audioSampleCollector.Length; e++)
+            {
+                RecordSampleValues(e);
+            }
         }
 
+        UpdateText();
     }
 
-    private void RecordSampleValues()
+    private void RecordSampleValues(int e)
     {
-        player1Index = player1AudioSampleCollector.indexOfHighestValue;
-        player1HighestValue = player1AudioSampleCollector.highestValue;
-        sourceIndex = sourceAudioSampleCollector.indexOfHighestValue;
-        sourceHighestValue = sourceAudioSampleCollector.highestValue;
+        index[e] = audioSampleCollector[e].indexOfHighestValue;
+        highestValue[e] = audioSampleCollector[e].highestValue;
+        //Debug.Log($"Highest Value of audio sample collector {e}: {highestValue[e]}");
 
-        differenceBetweenPlayer1AndSource = Math.Abs(sourceIndex - player1Index);
+        if (e > 0)
+        {
+            int playerSpecificIndex = e - 1;
+            differenceBetweenPlayerAndSource[playerSpecificIndex] = Math.Abs(index[0] - index[e]);
+            playerScore[playerSpecificIndex] += differenceBetweenPlayerAndSource[playerSpecificIndex];
+            Debug.Log($"Difference between player {e} and source: {differenceBetweenPlayerAndSource[playerSpecificIndex]}");
+        }
 
-        player1Score += differenceBetweenPlayer1AndSource;
+        //player1Index = player1AudioSampleCollector.indexOfHighestValue;
+        //player1HighestValue = player1AudioSampleCollector.highestValue;
+        //player2Index = player2AudioSampleCollector.indexOfHighestValue;
+
+        //sourceIndex = sourceAudioSampleCollector.indexOfHighestValue;
+        //sourceHighestValue = sourceAudioSampleCollector.highestValue;
+
+        //differenceBetweenPlayer1AndSource = Math.Abs(sourceIndex - player1Index);
+
+        //player1Score += differenceBetweenPlayer1AndSource;
     }
 
     private void UpdateText()
     {
-        if (player1HighestValue > 0.02f)
+        if (highestValue[1] > 0.02f)
         {
-            player1ScoreText.text = $"Player 1: {player1Score}";
+            player1ScoreText.text = $"Player 1 Score:\n{playerScore[0]}";
+            Debug.Log($"Player 1 score is {playerScore[0]}");
         }
 
-        player1DifferenceText.text = $"Difference between player 1 and source: {differenceBetweenPlayer1AndSource}";
+        if (highestValue[2] > 0.01f)
+            player2ScoreText.text = $"PLayer 2 Score:\n{playerScore[1]}";
+
+        player1DifferenceText.text = $"Difference between player 1 and source:\n{differenceBetweenPlayerAndSource[0]}";
+        player2DifferenceText.text = $"Difference between player 2 and source:\n{differenceBetweenPlayerAndSource[1]}";
+        //if (player1HighestValue > 0.02f)
+        //{
+        //    player1ScoreText.text = $"Player 1: {player1Score}";
+        //}
+
+            //player1DifferenceText.text = $"Difference between player 1 and source: {differenceBetweenPlayer1AndSource}";
     }
 }
