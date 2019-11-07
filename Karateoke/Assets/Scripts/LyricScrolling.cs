20ab2767ac96bf7fce678698dd04546174c4830b
Kat Keller <kat.kek.keller@gmail.com>
2019-11-06 20:51:07 -0600

    Added lyric scrolling script

---
+++ Karateoke/Assets/Scripts/LyricScrolling.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using System.Linq;
+
+public class LyricScrolling : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject wordPrefab;
+
+    [SerializeField]
+    private Vector3 spawningPosition;
+
+    [SerializeField]
+    private TextMeshProUGUI timerText;
+
+    [TextArea(5, 10)]
+    [SerializeField]
+    private string fullLyrics;
+
+    [SerializeField]
+    private float[] wordTimes = new float[14];
+
+    [SerializeField]
+    private float scrollTimeBuffer = 4.0f;
+
+    //private List<string> splitLyrics = new List<string>();
+    private string[] splitLyrics = new string[298];
+    private string[] wordTimeStrings = new string[14];
+    //private List<float> wordTimesList = new List<float>();
+    private List<string> wordTimesList = new List<string>();
+
+    private float timeElapsed;
+    private bool spawnWord;
+    private int indexOfWordToSpawn;
+    private TextMeshPro text;
+    private AudioSource audioSource;
+
+    private void Awake()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    void Start()
+    {
+        splitLyrics = fullLyrics.Split(' ');
+        //splitLyrics = splitLyricsArray.OfType<string>().ToList();
+
+        for (int i = 0; i < wordTimes.Length; i++)
+        {
+            wordTimes[i] -= scrollTimeBuffer;
+            wordTimeStrings[i] = wordTimes[i].ToString("F4");
+        }
+
+        //wordTimesList = wordTimes.OfType<float>().ToList();
+        wordTimesList = wordTimeStrings.OfType<string>().ToList();
+
+        audioSource.Play();
+    }
+
+    void Update()
+    {
+        timeElapsed += Time.deltaTime;
+        timerText.text = timeElapsed.ToString("F4");
+
+        spawnWord |= wordTimesList.Contains(timeElapsed.ToString("F4"));
+
+        if (spawnWord)
+        {
+            Debug.Log($"{splitLyrics[indexOfWordToSpawn]} should spawn");
+
+            GameObject a = Instantiate(wordPrefab) as GameObject;
+            a.transform.position = new Vector3(spawningPosition.x, spawningPosition.y, spawningPosition.z);
+            text = a.GetComponent<TextMeshPro>();
+            text.text = splitLyrics[indexOfWordToSpawn];
+            indexOfWordToSpawn++;
+        }
+    }
+}
---
+++ Karateoke/Assets/Scripts/ScrollingLyricBehavior.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrollingLyricBehavior : MonoBehaviour
+{
+    [SerializeField]
+    private float lerpTime = 4.0f;
+
+    [SerializeField]
+    private Vector3 endingLocation;
+
+    private Vector3 startingLocation;
+    private float currentLerpTime;
+
+    private void Start()
+    {
+        startingLocation = this.transform.position;
+    }
+
+    private void Update()
+    {
+        currentLerpTime += Time.deltaTime;
+
+        if (currentLerpTime >= lerpTime)
+        {
+            Destroy(this.gameObject);
+        }
+
+        float percentage = currentLerpTime / lerpTime;
+        this.transform.position = Vector3.Lerp(startingLocation, endingLocation, percentage);
+    }
+}
