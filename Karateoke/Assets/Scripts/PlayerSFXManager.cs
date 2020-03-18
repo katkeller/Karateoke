dc3f5bf4909e99fd81fa11ad07fb4d5d994ca465
Kat Keller <kat.kek.keller@gmail.com>
2020-03-18 18:30:01 -0500

    Created PlayerSFXManager, added some sound effects (likely placeholders)

---
+++ Karateoke/Assets/Scripts/PlayerSFXManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerSFXManager : MonoBehaviour
+{
+    [SerializeField]
+    private AudioClip attackClip, sweepClip, blockClip;
+
+    [SerializeField]
+    private AudioClip successfullyBlockClip, getHitClip;
+
+    private AudioSource audioSource;
+
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    public void AudioEvent_SuccessfullyBlock()
+    {
+        audioSource.PlayOneShot(successfullyBlockClip);
+    }
+
+    public void AudioEvent_Attack()
+    {
+        audioSource.PlayOneShot(attackClip);
+    }
+
+    public void AudioEvent_GetHit()
+    {
+        audioSource.PlayOneShot(getHitClip);
+    }
+}
