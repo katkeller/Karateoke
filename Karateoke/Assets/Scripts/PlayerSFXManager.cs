63acdcda391812aaba275300b2c9104e7f190de9
Kat Keller <kat.kek.keller@gmail.com>
2020-04-26 14:26:16 -0500

    Added camera shake to basic hit

--- Karateoke/Assets/Scripts/PlayerSFXManager.cs
+++ Karateoke/Assets/Scripts/PlayerSFXManager.cs
@@ -1,9 +1,16 @@
-﻿using System.Collections;
+﻿using Cinemachine;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerSFXManager : MonoBehaviour
 {
+    [SerializeField]
+    private CinemachineVirtualCamera mainCamera;
+
+    [SerializeField]
+    private float cameraShakeDuration = 0.2f, cameraShakeAmplitude = 1.0f, cameraShakeFrequency = 2.0f;
+
     [SerializeField]
     private AudioClip attackClip, sweepClip, blockClip;
 
@@ -14,10 +21,12 @@ public class PlayerSFXManager : MonoBehaviour
     private ParticleSystem kickTracer, blockParticles;
 
     private AudioSource audioSource;
+    private CinemachineBasicMultiChannelPerlin mainCameraNoise;
 
     void Start()
     {
         audioSource = GetComponent<AudioSource>();
+        mainCameraNoise = mainCamera.GetCinemachineComponent<CinemachineBasicMultiChannelPerlin>();
     }
 
     public void AudioEvent_Attack()
@@ -28,6 +37,7 @@ public class PlayerSFXManager : MonoBehaviour
     public void AudioEvent_GetHit()
     {
         audioSource.PlayOneShot(getHitClip);
+        StartCoroutine(ApplyCameraShake());
     }
 
     public void ActivateKickVFX()
@@ -48,5 +58,17 @@ public class PlayerSFXManager : MonoBehaviour
     {
         yield return new WaitForSeconds(secondsToWait);
         systemToStop.Stop(withChildren: true);
+    }
+
+    private IEnumerator ApplyCameraShake()
+    {
+        mainCameraNoise.m_AmplitudeGain = cameraShakeAmplitude;
+        mainCameraNoise.m_FrequencyGain = cameraShakeFrequency;
+
+        yield return new WaitForSeconds(cameraShakeDuration);
+
+        mainCameraNoise.m_AmplitudeGain = 0;
+        mainCameraNoise.m_FrequencyGain = 0;
+
     }
 }
