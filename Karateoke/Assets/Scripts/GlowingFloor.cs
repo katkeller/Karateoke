15baace700179790db80c9bed92ed7cf6f1a04ee
Kat Keller <kat.kek.keller@gmail.com>
2020-10-02 20:16:09 -0500

    Added glowing floor platform to main enviroment

---
+++ Karateoke/Assets/Scripts/GlowingFloor.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GlowingFloor : MonoBehaviour
+{
+    [SerializeField]
+    private Color secondColor;
+
+    private Color originalColor;
+    private Material material;
+
+    void Start()
+    {
+        material = GetComponent<MeshRenderer>().material;
+        originalColor = material.GetColor("_EmissionColor");
+    }
+
+    void Update()
+    {
+        LerpColor();
+    }
+
+    private void LerpColor()
+    {
+        var color = Color.Lerp(originalColor, secondColor, Mathf.PingPong(Time.time, 1));
+        material.SetColor("_EmissionColor", color);
+    }
+}
